de64ddccbdc4b3fe1a3dbe603fedcbb2cd3b5888
Stephan Heinemann <stephan@rigiresearch.com>
2019-09-12 18:13:55 -0700

    added ATPL folder

---
+++ ATPL/ATPL.mm
@@ -0,0 +1,251 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1568171896215" ID="ID_207039425" MODIFIED="1568174283124" TEXT="ATPL">
+<node CREATED="1568171989853" ID="ID_1998192836" MODIFIED="1568243332833" POSITION="right" TEXT="SARON">
+<node CREATED="1568172948798" ID="ID_62855509" MODIFIED="1568251888907" TEXT="Air Law and Procedures">
+<node CREATED="1568245279945" ID="ID_1079797516" MODIFIED="1568245319335" TEXT="1.05 Aircraft Type Ratings">
+<node CREATED="1568245337961" ID="ID_75602462" MODIFIED="1568246099441">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Individual Aircraft Type Ratings
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      <font size="3">Culhane 2020 Page 13 Addition (Detailed on Page 15):</font>
+    </p>
+    <font size="3">&quot;High performance aeroplane&quot; - means an aeroplane requiring a minimum crew of one pilot and having a VNE of 250 knots or greater, or a VSO of 80 knots or greater.</font>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1568248593919" ID="ID_786473397" MODIFIED="1568248615351" TEXT="1.01 Airline Transport License - Aeroplane"/>
+<node CREATED="1568248623793" ID="ID_931108479" MODIFIED="1568248633722" TEXT="1.02 Medical Requirements"/>
+<node CREATED="1568248647568" ID="ID_829787578" MODIFIED="1568248662339" TEXT="1.03 Pilot Licence Privileges"/>
+<node CREATED="1568248671336" ID="ID_1155718466" MODIFIED="1568248678874" TEXT="1.04 Aeroplane Class Ratings"/>
+<node CREATED="1568248686329" ID="ID_162885590" MODIFIED="1568248696287" TEXT="1.05 Aircraft Type Ratings"/>
+<node CREATED="1568248706165" ID="ID_314960253" MODIFIED="1568248713473" TEXT="1.06 Instrument Rating"/>
+<node CREATED="1568248720694" ID="ID_544994370" MODIFIED="1568248734245" TEXT="1.07 CARs General Provisions">
+<node CREATED="1568251901677" ID="ID_228267398" MODIFIED="1568327093033">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Operation of a Leased Aircraft by a Non-Registered Owner
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        <font size="3">Obtain authorization from the minister and inform minister in writing within 7 days after the term of lease commences. </font>
+      </li>
+      <li>
+        <font size="3">Maximum Number of Leased Aircraft &lt;= 25% </font>
+      </li>
+      <li>
+        <font size="3">Maximum Lease Period &lt;25 months within 30 consecutive months</font>
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1568327213275" ID="ID_666978895" MODIFIED="1568327228722" TEXT="1.08 Commercial Air Services and Flight Duty Rules">
+<node CREATED="1568327234243" ID="ID_1382274443" MODIFIED="1568327353767">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Small / Large Aeroplane
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        <font size="3">maximum permissible take-off weight 5700 kg (12566 lbs)</font>
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1568327544891" ID="ID_1922978008" MODIFIED="1568336883235">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Flight Duty Time / Limitations
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        <font size="3">includes 15 min post flight duties</font>
+      </li>
+      <li>
+        <font size="3">max. 14 consecutive hours in 24 consecutive hours</font>
+      </li>
+      <li>
+        <font size="3">min. 24 consecutive hours free from duty following 3 consecutive assignments exceeding 12 consecutive hours</font>
+      </li>
+      <li>
+        <font size="3">extension for half the rest period within duty time up to max. 3 hours possible if notified in advance</font>
+      </li>
+      <li>
+        <font size="3">extension for max. 3 hours in unforseen operational circumstances after consultation with flight crew</font>
+      </li>
+      <li>
+        <font size="3">air taxi: min. 24 consecutive hours free from duty, 13 times within 90 consecutive days, and 3 times within 30 consecutive days</font>
+      </li>
+      <li>
+        <font size="3">commuter or airline: min. 36 consecutive hours free from duty within 7 consecutive days, or 3 consecutive days within 17 consecutive days</font>
+      </li>
+      <li>
+        <font size="3">on call: min. 36 consecutive hours free from duty within 7 consecutive days, or 3 consecutive days with 17 consecutive days</font>
+      </li>
+      <li>
+        <font size="3">exceptions may be authorized in the AOC</font>
+      </li>
+    </ul>
+    <p>
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1568327614580" ID="ID_114771698" MODIFIED="1568327782122">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Flight Crew Member&#160;Status
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        <font size="3">On Standby: specific location and report within &lt;= 1h </font>
+      </li>
+      <li>
+        <font size="3">On Call: report within &lt;= 1h </font>
+      </li>
+      <li>
+        <font size="3">On Reserve: report within &gt; 1h</font>
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1568327837237" ID="ID_176350889" MODIFIED="1568327957445">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Minimum Rest Period
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        <font size="3">uninterrupted 8 h of consecutive sleep in suitable accomodation </font>
+      </li>
+      <li>
+        <font size="3">time to commute </font>
+      </li>
+      <li>
+        <font size="3">time for hygiene and meals</font>
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1568334563134" ID="ID_1456146745" MODIFIED="1568335090868">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Flight Time Limitations
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        <font size="3">max. 1200 hours in 365 consecutive days </font>
+      </li>
+      <li>
+        <font size="3">max. 300 hours in 90 consecutive days </font>
+      </li>
+      <li>
+        <font size="3">max. 120 hours in 30 consecutive days </font>
+      </li>
+      <li>
+        <font size="3">max. 100 hours in 30 consecutive days if on call </font>
+      </li>
+      <li>
+        <font size="3">max. 60 hours in 7 consecutive days if air taxi </font>
+      </li>
+      <li>
+        <font size="3">max. 40 hours in 7 consecutive days if commuter or airline</font>
+      </li>
+      <li>
+        <font size="3">max. 8 hours in 24 consecutive hours if single-pilot IFR</font>
+      </li>
+      <li>
+        <font size="3">exceptions may be authorized in the AOC</font>
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1568173327294" ID="ID_1056315969" MODIFIED="1568173426446" TEXT="Aircraft, Power Plants, Propellers and Systems"/>
+<node CREATED="1568173515599" ID="ID_228371769" MODIFIED="1568173519521" TEXT="Instruments"/>
+<node CREATED="1568173570034" ID="ID_1285060949" MODIFIED="1568173574370" TEXT="Navigation"/>
+<node CREATED="1568173802936" ID="ID_851590336" MODIFIED="1568173809889" TEXT="Flight Operations"/>
+<node CREATED="1568173868337" ID="ID_1290650653" MODIFIED="1568249056208" TEXT="Theory of Flight">
+<icon BUILTIN="closed"/>
+</node>
+<node CREATED="1568173906119" ID="ID_1943574224" MODIFIED="1568249061487" TEXT="Human Factors">
+<icon BUILTIN="closed"/>
+</node>
+</node>
+<node CREATED="1568172247782" ID="ID_1312210863" MODIFIED="1568243307685" POSITION="left" TEXT="SAMRA">
+<node CREATED="1568172518654" FOLDED="true" ID="ID_1271575521" MODIFIED="1568174672093" TEXT="Meteorology">
+<node CREATED="1568174438816" ID="ID_208638027" MODIFIED="1568174450928" TEXT="The Earth&apos;s Atmosphere"/>
+</node>
+<node CREATED="1568172603794" ID="ID_1873206513" MODIFIED="1568249087804" TEXT="Flight Planning">
+<icon BUILTIN="closed"/>
+</node>
+<node CREATED="1568172671136" ID="ID_1053951536" MODIFIED="1568172701231" TEXT="Radio Communication and Navigation Aids"/>
+</node>
+</node>
+</map>
