ad566821b889fb0994232a07caef8347e7939d14
Stephan Heinemann <stephan@rigiresearch.com>
2020-01-23 13:45:59 -0800

    added IFR mind map

---
+++ MIFR/IFR.mm
@@ -0,0 +1,564 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1579633293459" ID="ID_637111351" MODIFIED="1579633300923" TEXT="IFR">
+<node CREATED="1579633398684" FOLDED="true" ID="ID_59338447" MODIFIED="1579811131078" POSITION="right" TEXT="General">
+<node CREATED="1579644863953" ID="ID_1122055925" MODIFIED="1579644877096" TEXT="Air Traffic Control (ATC) Clearance"/>
+<node CREATED="1579644898252" FOLDED="true" ID="ID_1705905435" MODIFIED="1579811125587">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      IFR Flights in VMC
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1579644938247" ID="ID_103493367" MODIFIED="1579644955500" TEXT="IFR Clearance with VFR Restrictions"/>
+<node CREATED="1579645004580" ID="ID_1584017312" MODIFIED="1579645012325" TEXT="VFR Release of an IFR Aircraft"/>
+</node>
+<node CREATED="1579645051864" FOLDED="true" ID="ID_896462591" MODIFIED="1579811123351" TEXT="Emergencies and Equipment Failures">
+<node CREATED="1579645094725" ID="ID_1346994412" MODIFIED="1579645102022" TEXT="Declaration of Emergency"/>
+<node CREATED="1579645115872" ID="ID_1851039404" MODIFIED="1579645123705" TEXT="Two-Way Communications Failure"/>
+<node CREATED="1579645186019" ID="ID_1781015447" MODIFIED="1579645198007" TEXT="Reporting Malfunctions of Navigation and Communications Equipment"/>
+</node>
+<node CREATED="1579635981458" ID="ID_831485465" MODIFIED="1579636095907">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Fuel Dumping
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        should be carried out at least <b>2000</b>&#160;ft above highest obstacle within <b>5</b>&#160;NM
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1579645256451" FOLDED="true" ID="ID_355525268" MODIFIED="1579811119354" TEXT="IFR Separation">
+<node CREATED="1579636165195" ID="ID_316886654" MODIFIED="1579645293475">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Vertical Separation - General
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        <b>1000</b>&#160;ft at and below FL <b>290</b>
+      </li>
+      <li>
+        <b>2000</b>&#160;ft above FL <b>290</b>&#160;(non-RVSM)
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1579636310885" ID="ID_1303080214" MODIFIED="1579636939661">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Vertical Separation between FL and ASL
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        lowest usable FL depending on altimeter setting (high-level airspace)
+
+        <ul>
+          <li>
+            <b>29.92</b>&#160;and above: FL <b>180</b>
+          </li>
+          <li>
+            <b>28.92</b>&#160;to <b>29.91</b>: FL <b>190</b>
+          </li>
+          <li>
+            <b>27.92</b>&#160;to <b>28.91</b>: FL <b>200</b>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1579645338514" ID="ID_279060898" MODIFIED="1579645348323" TEXT="Longitudinal Separation Distance-Based"/>
+<node CREATED="1579645368660" ID="ID_372454248" MODIFIED="1579645374412" TEXT="Lateral Separation - General"/>
+<node CREATED="1579636651467" ID="ID_1756654326" MODIFIED="1579636982763">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Lateral Separation - Airways and Tracks
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        entire width of airway or track
+      </li>
+      <li>
+        off-airway tracks
+
+        <ul>
+          <li>
+            <b>45</b>&#160;NM either side of centerline beyond navigational and signal coverage range
+          </li>
+          <li>
+            additional protection area on maneuvering side for track changes exceeding <b>15</b>&#160;degrees at or above FL <b>180</b>&#160;(high-level airspace)
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1579645471644" ID="ID_1650399377" MODIFIED="1579645486723" TEXT="Lateral Separation - Instrument Approach Procedure (IAP)"/>
+</node>
+<node CREATED="1579637145918" FOLDED="true" ID="ID_288271749" MODIFIED="1579811129063">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Visual Separation
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        may be applied below <b>12500</b>&#160;ft ASL, in CZ or TCA
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<node CREATED="1579645598377" ID="ID_7002096" MODIFIED="1579645607209" TEXT="Speed Control Instructions on Departure"/>
+<node CREATED="1579645624161" ID="ID_354664651" MODIFIED="1579645641473">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Controller-Applied Visual Separation
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1579645644538" ID="ID_1796439549" MODIFIED="1579645652153" TEXT="Pilot-Applied Visual Separation"/>
+</node>
+<node CREATED="1579645689253" ID="ID_80744334" MODIFIED="1579645696519" TEXT="Development of Instrument Procedures"/>
+</node>
+<node CREATED="1579633414558" FOLDED="true" ID="ID_654204230" MODIFIED="1579811178893" POSITION="left" TEXT="Departure Procedures">
+<node CREATED="1579645753810" ID="ID_1034102258" MODIFIED="1579645765446" TEXT="Aerodrome Operations"/>
+<node CREATED="1579645777165" ID="ID_948846528" MODIFIED="1579645790319" TEXT="Automatic Terminal Information System (ATIS) Broadcast"/>
+<node CREATED="1579645820876" ID="ID_1542868492" MODIFIED="1579645824944" TEXT="Initial Contact"/>
+<node CREATED="1579645828133" ID="ID_72886895" MODIFIED="1579645831414" TEXT="IFR Clearances"/>
+<node CREATED="1579645846990" ID="ID_425430365" MODIFIED="1579645964937">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Standard Instrument Departure (SID)
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        pilot navigation versus vectored
+      </li>
+      <li>
+        initial and second segment
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1579646030855" FOLDED="true" ID="ID_1367856101" MODIFIED="1579811150662" TEXT="Noise Abatement Procedures - Departure">
+<node CREATED="1579646068429" ID="ID_955884881" MODIFIED="1579646074672" TEXT="Noise-Preferential Runways"/>
+<node CREATED="1579640103681" FOLDED="true" ID="ID_1376215789" MODIFIED="1579646128851">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Noise Abatement Departure Procedures (NADP)
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        commence not below <b>800</b>&#160;ft AAE
+      </li>
+      <li>
+        terminate not above <b>3000</b>&#160;ft AAE
+      </li>
+      <li>
+        initial climb to <b>800</b>&#160;ft AAE with at least <b>V2 + 10</b>
+      </li>
+      <li>
+        <b>Vzf</b>&#160;- zero flap minimum control speed
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<node CREATED="1579640758138" ID="ID_1727213715" MODIFIED="1579641862595">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      NADP1
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        at <b>V2 + 10 </b>to <b>20</b>&#160;to initiation point
+      </li>
+      <li>
+        at <b>800</b>&#160;ft AAE power reduction maintaining positive rate at <b>V2 + 10</b>&#160;to <b>20</b>&#160;&#160;with flaps / slats in take-off configuration
+      </li>
+      <li>
+        at <b>3000</b>&#160;ft AAE retraction of flaps / slats maintaining positive rate and transition to en-route climb
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1579640767156" ID="ID_950172458" MODIFIED="1579641956913">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      NADP2
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        at <b>V2 + 10</b>&#160;to <b>20</b>&#160;to initiation point
+      </li>
+      <li>
+        at or above <b>800</b>&#160;ft AAE acceleration to <b>Vzf</b>&#160;maintaining positive rate with first stage flaps / slats retraction and power reduction, <i>or</i>
+      </li>
+      <li>
+        at or above <b>800</b>&#160;ft AAE retraction of flaps / slats and acceleration to <b>Vzf</b>&#160;+ <b>10</b>&#160;to <b>20</b>&#160;maintaining positive rate
+      </li>
+      <li>
+        at <b>3000</b>&#160;ft AAE transition to en-route climb
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1579642122387" FOLDED="true" ID="ID_15581158" MODIFIED="1579646185381">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Obstacle and Terrain Clearance
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        Standard Instrument Departure (SID) versus Obstacle Departure Procedure (ODP)
+      </li>
+      <li>
+        ODP requirements
+
+        <ul>
+          <li>
+            <b>35</b>&#160;ft above departure end of runway (<b>V2</b>&#160;at <b>35</b>&#160;HAT)
+          </li>
+          <li>
+            <b>400</b>&#160;ft AAE before initiating turns (<b>V3</b>&#160;at safe / cleanup altitude)
+          </li>
+          <li>
+            <b>200</b>&#160;ft / NM climb gradient
+          </li>
+        </ul>
+      </li>
+      <li>
+        ODP types
+
+        <ul>
+          <li>
+            <b>1/2</b>&#160;SM: take-off and climb on course
+          </li>
+          <li>
+            <b>*</b>&#160;(SPEC VIS)
+          </li>
+          <li>
+            not assessed: PIC responsible for obstacle clearance
+          </li>
+        </ul>
+      </li>
+      <li>
+        at least <b>1/2</b>&#160;SM (RVR 2600) if not specified otherwise
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<node CREATED="1579642903721" ID="ID_284205814" MODIFIED="1579643455694">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      SPEC VIS Categories
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        according to aircraft approach speed category
+      </li>
+      <li>
+        A: <b>1</b>&#160;SM
+      </li>
+      <li>
+        B: <b>1</b>&#160;<b>1/2</b>&#160;SM
+      </li>
+      <li>
+        C: <b>2</b>&#160;SM
+      </li>
+      <li>
+        D: <b>2</b>&#160;SM
+      </li>
+      <li>
+        E: not specified
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1579644148784" ID="ID_1592203946" MODIFIED="1579644160675" TEXT="Visual Climb Over the Airport (VCOA)"/>
+<node CREATED="1579644309409" ID="ID_400851938" MODIFIED="1579644334404">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Low, Close-In Obstacles
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        Obstacle Clearance Surface (OCS)
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1579644396728" ID="ID_1220853600" MODIFIED="1579644402728" TEXT="Release from Tower Frequency"/>
+<node CREATED="1579644466100" ID="ID_1705369011" MODIFIED="1579644474483" TEXT="Departures from Uncontrolled Airports"/>
+<node CREATED="1579644760443" ID="ID_77314256" MODIFIED="1579644770079" TEXT="Alerting Service for Uncontrolled Airports"/>
+</node>
+<node CREATED="1579633433848" FOLDED="true" ID="ID_1247278460" MODIFIED="1579815780664" POSITION="right" TEXT="En Route Procedures">
+<node CREATED="1579811215993" ID="ID_1170940779" MODIFIED="1579811219032" TEXT="Position Reports"/>
+<node CREATED="1579811805490" ID="ID_1221424960" MODIFIED="1579812031916">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Mach Number / TAS - Clearances and Reports
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        ATC is to be notified if airspeed changes by
+      </li>
+      <li>
+        more than <b>0.01</b>&#160;M, or
+      </li>
+      <li>
+        <b>5</b>%&#160;TAS or more
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1579812064801" ID="ID_1599666678" MODIFIED="1579812068098" TEXT="Altitude Reports"/>
+<node CREATED="1579812268334" FOLDED="true" ID="ID_1339956055" MODIFIED="1579813319579" TEXT="Climb or Descent">
+<node CREATED="1579812603291" ID="ID_189299697" MODIFIED="1579812609263" TEXT="Visual Climb and Descent"/>
+</node>
+<node CREATED="1579812710027" ID="ID_1761413884" MODIFIED="1579814690003">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Minimum IFR Altitudes
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        <b>1000</b>&#160;ft within <b>5</b>&#160;NM of the highest obstacle
+      </li>
+      <li>
+        <b>1500</b>&#160;ft within <b>5</b>&#160;NM of the highest obstacle in designated mountainous regions 2, 3, 4
+      </li>
+      <li>
+        <b>2000</b>&#160;ft within <b>5</b>&#160;NM of the highest obstacle in designated mountainous regions 1, 5
+      </li>
+      <li>
+        AMA, SALT 100NM, MOCA, MEA, MVA, MSA, MDA, DA
+      </li>
+      <li>
+        higher MEA required for fix joining legs with different MEAs
+      </li>
+      <li>
+        <b>1000</b>&#160;ft above MEA in extreme cold weather
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1579813700085" ID="ID_1129084101" MODIFIED="1579813705668" TEXT="ATC Assignment of Altitudes">
+<node CREATED="1579814173974" ID="ID_1577062654" MODIFIED="1579814179584" TEXT="Minimum IFR Altitude"/>
+<node CREATED="1579814200983" ID="ID_1216937735" MODIFIED="1579814663894">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Altitudes and Direction of Flight
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        offset tracks in high-level airspace for non-standard altitudes
+      </li>
+      <li>
+        at least <b>5</b>&#160;NM parallel offset track in special circumstances (e.g., icing, failures)
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1579814734237" ID="ID_1535449685" MODIFIED="1579815049227">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1000 ft On-Top IFR Flight
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        <b>1000</b>&#160;ft on top of well-defined layer (e.g., haze, cloud)
+      </li>
+      <li>
+        in airspace B at or below <b>12500</b>&#160;ft ASL
+      </li>
+      <li>
+        in airspace C, D, E
+      </li>
+      <li>
+        no ATC separation (except hold and altitude reservation)
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1579815238411" ID="ID_1474722692" MODIFIED="1579815247343" TEXT="Clearances - Leaving or Entering Controlled Airspace"/>
+<node CREATED="1579815331121" ID="ID_1845828154" MODIFIED="1579815335324" TEXT="Clearance Limit"/>
+<node CREATED="1579815402534" ID="ID_1882447188" MODIFIED="1579815738291">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Class G Airspace - Recommended Operating Procedures - En-Route
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        126.7 MHz for reports and intentions
+      </li>
+      <li>
+        over all NAVAIDs
+      </li>
+      <li>
+        before approach and switching to MF
+      </li>
+      <li>
+        before take-off in addition to MF
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1579633454131" ID="ID_307271175" MODIFIED="1579633457973" POSITION="left" TEXT="Arrival Procedures"/>
+<node CREATED="1579633476953" ID="ID_66824391" MODIFIED="1579633482399" POSITION="right" TEXT="Holding Procedures"/>
+<node CREATED="1579815802154" ID="ID_971957458" MODIFIED="1579815814323" POSITION="left" TEXT="Practical Procedures"/>
+</node>
+</map>
