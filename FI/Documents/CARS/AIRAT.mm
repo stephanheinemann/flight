4c1961410d17172c954fe15ff8d1b92356697cc1
Stephan Heinemann <stephan@rigiresearch.com>
2020-10-03 11:36:30 -0700

    added AIRAT mind map

---
+++ FI/Documents/CARS/AIRAT.mm
@@ -0,0 +1,37 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1601686337679" ID="ID_1651238697" MODIFIED="1601686355049">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      AIRAT
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1601686463519" ID="ID_1382913323" MODIFIED="1601686475036" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Flight Instructor Guide
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1601686478539" FOLDED="true" ID="ID_446566040" MODIFIED="1601687106148" POSITION="left" TEXT="CARs">
+<node CREATED="1601687030791" ID="ID_1799555625" MODIFIED="1601687033959" TEXT="Part IV"/>
+<node CREATED="1601687037693" ID="ID_35577576" MODIFIED="1601687041603" TEXT="Part VI"/>
+</node>
+<node CREATED="1601686500988" ID="ID_739673680" MODIFIED="1601686504167" POSITION="right" TEXT="Standards"/>
+<node CREATED="1601687056181" FOLDED="true" ID="ID_1522117251" MODIFIED="1601687106149" POSITION="left" TEXT="Practice Exams">
+<node CREATED="1601687084749" ID="ID_230562098" MODIFIED="1601687089374" TEXT="Culhane"/>
+</node>
+</node>
+</map>
