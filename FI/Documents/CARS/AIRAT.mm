a2ba7177fcb4a4349b0b858d9630d0dc20653195
Stephan Heinemann <stephan@rigiresearch.com>
2020-10-09 16:41:48 -0700

    extended AIRAT mind map

--- FI/Documents/CARS/AIRAT.mm
+++ FI/Documents/CARS/AIRAT.mm
@@ -10,8 +10,7 @@
       AIRAT
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1601686463519" ID="ID_1382913323" MODIFIED="1601686475036" POSITION="right">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -22,11 +21,136 @@
       Flight Instructor Guide
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
-<node CREATED="1601686478539" FOLDED="true" ID="ID_446566040" MODIFIED="1601687106148" POSITION="left" TEXT="CARs">
-<node CREATED="1601687030791" ID="ID_1799555625" MODIFIED="1601687033959" TEXT="Part IV"/>
+<node CREATED="1601686478539" FOLDED="true" ID="ID_446566040" MODIFIED="1602286810135" POSITION="left" TEXT="CARs">
+<node CREATED="1601687030791" ID="ID_1799555625" MODIFIED="1601687033959" TEXT="Part IV">
+<node CREATED="1601927950343" ID="ID_500012379" MODIFIED="1601928484704">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Time Limits (400.03)
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        <b>24</b>&#160;months for written examinations, with exceptions:
+
+        <ul>
+          <li>
+            student pilot permit
+          </li>
+          <li>
+            <b>70</b>% or higher obtained for type rating
+          </li>
+          <li>
+            <b>70</b>% or higher obtained for senior commercial pilot
+          </li>
+          <li>
+            <b>5</b>&#160;years for integrated ATPL written
+          </li>
+        </ul>
+      </li>
+      <li>
+        <b>12</b>&#160;months for AME following the date of license application
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1601928318731" ID="ID_632412376" MODIFIED="1601928448618">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Rewriting of Examinations (400.04)
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        <b>14</b>&#160;days after first failure
+      </li>
+      <li>
+        <b>30</b>&#160;days after second failure
+      </li>
+      <li>
+        <b>30</b>&#160;days + <b>30</b>&#160;days for each failure in excess of two
+      </li>
+      <li>
+        <b>180</b>&#160;days maximum
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1601928721719" ID="ID_196376399" MODIFIED="1601928842627">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Change of Address (400.07)
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        <b>7</b>&#160;days for permit or license holder to notify TC after change
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1602277603913" ID="ID_1138897048" MODIFIED="1602278414525">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Recency Requirements (401.05)
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        <b>5</b>&#160;years recency
+      </li>
+      <li>
+        <b>12</b>&#160;months (annual) flight review
+      </li>
+      <li>
+        <b>24</b>&#160;months currency
+      </li>
+      <li>
+        <b>6</b>&#160;months, <b>5</b>&#160;take-offs and landings (day or night) PAX currency
+      </li>
+      <li>
+        <b>24</b>&#160;months instrument currency (IPC)
+      </li>
+      <li>
+        after <b>12</b>&#160;months:<b>&#160;6 </b>months, <b>6 </b>hours, <b>6</b>&#160;approaches
+      </li>
+      <li>
+        <b>3</b>&#160;years retaining instrument training records
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+</node>
 <node CREATED="1601687037693" ID="ID_35577576" MODIFIED="1601687041603" TEXT="Part VI"/>
 </node>
 <node CREATED="1601686500988" ID="ID_739673680" MODIFIED="1601686504167" POSITION="right" TEXT="Standards"/>
